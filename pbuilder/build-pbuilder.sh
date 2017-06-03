6ada629111891d0d8f908e66973aaffab2efdb7c
JBBgameich <jbbgameich@outlook.com>
2017-06-03 14:27:51 +0200

    Remove unused options on pbuilder create

--- pbuilder/build-pbuilder.sh
+++ pbuilder/build-pbuilder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pbuilder create --distribution stretch --mirror http://deb.debian.org/debian --basetgz stretch.tgz --save-after-login --save-after-exec
+sudo pbuilder create --distribution stretch --mirror http://deb.debian.org/debian --basetgz stretch.tgz
 sudo pbuilder execute --basetgz stretch.tgz --save-after-exec --save-after-login -- hooks/00-configure-apt.sh
