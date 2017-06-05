c4a02b638e9f809238b3ec38e712b5504064a973
JBBgameich <jbbgameich@outlook.com>
2017-06-05 16:08:06 +0200

    Simplify build result folder

--- pbuilder/build-packages.sh
+++ pbuilder/build-packages.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-sudo rm /var/cache/pbuilder/result/*
+mkdir ../result
 
 for i in ../packages/source/*.dsc
 do
     if test -f "$i" 
     then
        echo "Building source file $i in pbuilder"
-       sudo pbuilder build --basetgz stretch.tgz $i
+       sudo pbuilder build --basetgz stretch.tgz --buildresult ../result $i
     fi
-done 
-
-mkdir ../result
-cp /var/cache/pbuilder/result/* ../result
+done
